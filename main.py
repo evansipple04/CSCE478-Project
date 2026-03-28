bd7c5100bd2ac2255a499121c3f47550fb1c0e14
esipple2 <esipple2@huskers.unl.edu>
2026-03-28 15:24:16 -0500

    outputting data before and after preprocessing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from src.load_data import load_csv, load_multiple_csvs
+from src.load_data import load_csv
 from src.train_models import ModelTrainer
 from src.preprocess import preprocess_data
+from src.train_models import ModelTrainer
 from pathlib import Path
 
 def main():
     df_weather = load_csv(Path("data/weather.csv"))
-    df_rides = load_csv(Path("data/rides.csv"))
-
+    df_rides = load_csv(Path("data/cab_rides.csv"))
+    print(f"data before preprocessing: {df_weather.shape[0]} weather rows, {df_rides.shape[0]} rides rows")
     df_weather, df_rides = preprocess_data(df_weather, df_rides)
+    print(f"data after preprocessing: {df_weather.shape[0]} weather rows, {df_rides.shape[0]} rides rows")
+
+if __name__ == "__main__":
+    main()
+    
